9be8a3a2149ee06c4e524b77923597c82cd1bf66
Josh <joshuastolle445@gmail.com>
2023-06-26 11:39:25 +0200

    add insane table

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -3,16 +3,20 @@ const Resume = () => {
     return <article>
         <h2>Antonius Maximus</h2>
         <h3>Roman Emperor</h3>
-        <table>
+        <table style={{border: "deepskyblue solid 1px"}}>
             <thead>
             <tr>
                 <th colSpan={2}>Experience</th>
             </tr>
             </thead>
-            <tbody>
+            <tbody style={{background: "hotpink"}}>
                 <tr>
-                    <td></td>
-                    <td></td>
+                    <td>86 BC</td>
+                    <td>Born</td>
+                </tr>
+                <tr>
+                    <td>30 BC</td>
+                    <td>Died</td>
                 </tr>
             </tbody>
         </table>
